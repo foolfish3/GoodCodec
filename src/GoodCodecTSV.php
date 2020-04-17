8ec1fec0b243c2b2a2c69a4dd31ee27f9004b385
qian.yu <foolfish@188.com>
2020-04-17 21:50:13 +0800

    commit

--- src/GoodCodecTSV.php
+++ src/GoodCodecTSV.php
@@ -80,7 +80,7 @@ class GoodCodecTSV{
 		}
 	}
 
-	public static function tsv_decode_stream($stream, $close_stream,$skip_lines=0,$in_charset="UTF-8", $out_charset="UTF-8",$remove_bom=0){
+    public static function tsv_decode_stream($stream, $close_stream,$skip_lines=0,$in_charset="UTF-8", $out_charset="UTF-8",$remove_bom=0){
 		static $map=array(0=>0,1=>1,2=>2,3=>3,4=>4,5=>5,6=>6,7=>7,8=>8,9=>9,"a"=>10,"A"=>10,"b"=>11,"B"=>11,"c"=>12,"C"=>12,"d"=>13,"D"=>13,"e"=>14,"E"=>14,"f"=>15,"F"=>15);
 		static $map2=array("b"=>"\x08","f"=>"\x0c","r"=>"\r","n"=>"\n","t"=>"\t","0"=>"\x00","'"=>"'","\\"=>"\\","a"=>"\x07","v"=>"\x0b");
 		if($out_charset==="UTF-8"||$out_charset===NULL||isset(self::$utf8_map[$out_charset])){
@@ -103,13 +103,12 @@ class GoodCodecTSV{
 				\fclose($stream);
 			}
 			return;
-		}
+        }
 		$row=array();
 		$s="";
-		$state=0;
+        $state=0;
 		for(;;){
-			($c=\fgetc($stream))!==false or $c="";
-RESEND:		switch($state){
+    		switch($state){
 				case 0:
 					switch($c){
 						case "":
@@ -121,7 +120,8 @@ RESEND:		switch($state){
 							}
 							break 2;//END no \n but END
 						case "\\":
-							$state=1;
+                            $state=1;
+                            ($c=\fgetc($stream))!==false or $c="";
 						break;
 						case "\r"://过滤掉后面N个\n
 							for(;;){
@@ -141,11 +141,11 @@ RESEND:		switch($state){
 							if($c===""){
 								break 3;//NORMAL END
 							}
-							goto RESEND;
 						break;
 						case "\t":
 							$row[]=$need_iconv&&$s!==NULL?\iconv("UTF-8",$out_charset,$s):$s;
-							$s="";
+                            $s="";
+                            ($c=\fgetc($stream))!==false or $c="";
 						break;
 						case "\n"://过滤掉后面N个\r
 							for(;;){
@@ -165,7 +165,6 @@ RESEND:		switch($state){
 							if($c===""){
 								break 3;//NORMAL END
 							}
-							goto RESEND;
 						break;
 						default:
 							if($detect_bom && $c===$detect_bom){
@@ -180,12 +179,13 @@ RESEND:		switch($state){
 									if($c===""){
 										break 3;
 									}
-									continue 3;
+                                    break;
 								}
 							}else{
 								$detect_bom=NULL;
-							}
-							$s.=$c;
+                            }
+                            $s.=$c;
+                            ($c=\fgetc($stream))!==false or $c="";
 					}
 				break;
 				case 1:
@@ -201,22 +201,39 @@ RESEND:		switch($state){
 					}
 					if(isset($map2[$c])){
 						$s.=$map2[$c];
-						$state=0;
+                        $state=0;
+                        ($c=\fgetc($stream))!==false or $c="";
 					}else if($c=="x"){
-						$state=2;
+                        $state=2;
+                        ($c=\fgetc($stream))!==false or $c="";
 					}else if($c=="N"){
 						if($s!==""){//TODO check it \N in string,what will happen
-							\trigger_error("\\N in string, it's not a normal way");
 							$s.=$c;
-							$state=0;
+                            $state=0;
+                            ($c=\fgetc($stream))!==false or $c="";
 						}else{
-							$row[]=null;
-							$s="";
+                            ($c=\fgetc($stream))!==false or $c="";
+                            switch($c){
+                                case "":
+                                    $row[]=null;
+                                    if($skip_lines>0){
+                                        $skip_lines--;
+                                    }else{
+                                        (yield $row);
+                                    }
+                                    break 3;
+                                case "\r":case "\n":case "\t":
+                                    $s=null;
+                                    $state=0;
+                                    continue 3;
+                            }
+                            $s.="N";
 							$state=0;
 						}
 					}else{
 						$s.=$c;
-						$state=0;
+                        $state=0;
+                        ($c=\fgetc($stream))!==false or $c="";
 					}
 				break;
 				case 2:
@@ -231,7 +248,8 @@ RESEND:		switch($state){
 						break 2;//END last char is uncomplete \x
 					}
 					$oct=$map[$c]*16;//warning if not 0-9A-Fa-f
-					$state=3;
+                    $state=3;
+                    ($c=\fgetc($stream))!==false or $c="";
 				break;
 				case 3:
 					if($c===""){
@@ -245,19 +263,13 @@ RESEND:		switch($state){
 						break 2;//END last char is uncomplete \xN
 					}
 					$s.=\chr($oct+$map[$c]);//warning if not 0-9A-Fa-f
-					$state=0;
+                    $state=0;
+                    ($c=\fgetc($stream))!==false or $c="";
 				break;
 				default:
 					throw new \Error("BUG");
 			}
 		}
-		if($filter){
-			\stream_filter_remove($filter);
-		}
-		if($close_stream){
-			\fclose($stream);
-		}
-		return;
 	}
 
 	public static function tsv_decode_str($str,$skip_lines=0,$in_charset="UTF-8", $out_charset="UTF-8",$remove_bom=0){
@@ -275,15 +287,14 @@ RESEND:		switch($state){
 		$detect_bom= $remove_bom && $in_charset==="UTF-8"?"\xEF":NULL;
 		$need_iconv=$out_charset==="UTF-8";
 		if($in_charset!=="UTF-8"){
-			$str=iconv($in_charset,"UTF-8",$str);
+			$str=\iconv($in_charset,"UTF-8",$str);
 		}
-		$index=0;
 		$data=$row=array();
 		$s="";
-		$state=0;
+        $state=0;
+        $c=@$str[$index=0];
 		for(;;){
-			$c=@$str[++$index];
-RESEND:		switch($state){
+    		switch($state){
 				case 0:
 					switch($c){
 						case "":
@@ -295,7 +306,8 @@ RESEND:		switch($state){
 							}
 							break 2;//END no \n but END
 						case "\\":
-							$state=1;
+                            $state=1;
+                            $c=@$str[++$index];
 						break;
 						case "\r"://过滤掉后面N个\n
 							for(;;){
@@ -315,11 +327,11 @@ RESEND:		switch($state){
 							if($c===""){
 								break 3;//NORMAL END
 							}
-							goto RESEND;
 						break;
 						case "\t":
 							$row[]=$need_iconv&&$s!==NULL?\iconv("UTF-8",$out_charset,$s):$s;
-							$s="";
+                            $s="";
+                            $c=@$str[++$index];
 						break;
 						case "\n"://过滤掉后面N个\r
 							for(;;){
@@ -339,7 +351,6 @@ RESEND:		switch($state){
 							if($c===""){
 								break 3;//NORMAL END
 							}
-							goto RESEND;
 						break;
 						default:
 							if($detect_bom && $c===$detect_bom){
@@ -354,12 +365,13 @@ RESEND:		switch($state){
 									if($c===""){
 										break 3;
 									}
-									continue 3;
+                                    break;
 								}
 							}else{
 								$detect_bom=NULL;
-							}
-							$s.=$c;
+                            }
+                            $s.=$c;
+                            $c=@$str[++$index];
 					}
 				break;
 				case 1:
@@ -375,21 +387,39 @@ RESEND:		switch($state){
 					}
 					if(isset($map2[$c])){
 						$s.=$map2[$c];
-						$state=0;
+                        $state=0;
+                        $c=@$str[++$index];
 					}else if($c=="x"){
-						$state=2;
+                        $state=2;
+                        $c=@$str[++$index];
 					}else if($c=="N"){
 						if($s!==""){//TODO check it \N in string,what will happen
 							$s.=$c;
-							$state=0;
+                            $state=0;
+                            $c=@$str[++$index];
 						}else{
-							$row[]=null;
-							$s="";
+                            $c=@$str[++$index];
+                            switch($c){
+                                case "":
+                                    $row[]=null;
+                                    if($skip_lines>0){
+                                        $skip_lines--;
+                                    }else{
+                                        $data[]=$row;
+                                    }
+                                    break 3;
+                                case "\r":case "\n":case "\t":
+                                    $s=null;
+                                    $state=0;
+                                    continue 3;
+                            }
+                            $s.="N";
 							$state=0;
 						}
 					}else{
 						$s.=$c;
-						$state=0;
+                        $state=0;
+                        $c=@$str[++$index];
 					}
 				break;
 				case 2:
@@ -404,7 +434,8 @@ RESEND:		switch($state){
 						break 2;//END last char is uncomplete \x
 					}
 					$oct=$map[$c]*16;//warning if not 0-9A-Fa-f
-					$state=3;
+                    $state=3;
+                    $c=@$str[++$index];
 				break;
 				case 3:
 					if($c===""){
@@ -418,7 +449,8 @@ RESEND:		switch($state){
 						break 2;//END last char is uncomplete \xN
 					}
 					$s.=\chr($oct+$map[$c]);//warning if not 0-9A-Fa-f
-					$state=0;
+                    $state=0;
+                    $c=@$str[++$index];
 				break;
 				default:
 					throw new \Error("BUG");
