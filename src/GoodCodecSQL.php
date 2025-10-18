b0483b68c37ca344a4bd67a48ee360af1893904c
钱宇 <qian.yu@nint.com>
2025-10-18 17:18:27 +0800

    commit

--- src/GoodCodecSQL.php
+++ src/GoodCodecSQL.php
@@ -337,6 +337,9 @@ class GoodCodecSQL
     {
         $args = \func_get_args();
         $bind_param = array();
+        if (\is_array($args[0])) {
+            $args = $args[0];
+        }
         if (isset($args[1]) && is_array($args[1])) {
             foreach ($args[1] as $k => $v) {
                 if (\preg_match("{^\\d+$}", $k)) {
@@ -350,7 +353,7 @@ class GoodCodecSQL
                 $bind_param[$i] = $args[$i];
             }
         }
-        $ss = is_string($args[0])? self::sql_token_get_all($args[0]): $args[0];
+        $ss = self::sql_token_get_all($args[0]);
         //?
         //?s
         //?s?gogo
